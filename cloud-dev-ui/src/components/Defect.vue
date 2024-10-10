fb36d3da32cdd41dc3d14646069474e671ef3cf9
Ostabo <osi.borkenhagen@gmail.com>
2024-10-10 18:21:13 +0200

    Changes by Lars

--- cloud-dev-ui/src/components/Defect.vue
+++ cloud-dev-ui/src/components/Defect.vue
@@ -1,11 +1,30 @@
 <template>
-  <v-card />
+  <v-card
+    class="mx-auto my-4"
+    elevation="16"
+    max-width="1000"
+  >
+    <v-card-item>
+      <v-card-title>
+        {{ msg }}
+      </v-card-title>
+
+      <v-card-subtitle>
+        {{ descr }}
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </v-card-text>
+  </v-card>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+
+  defineProps({
+    msg: String,
+    descr: String,
+  })
 
 </script>
-
-<style scoped lang="sass">
-
-</style>
