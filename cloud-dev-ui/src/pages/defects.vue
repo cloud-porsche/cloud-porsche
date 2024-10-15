75253dd9900f037ba4402a1227cd9a06988f6dfd
Ostabo <osi.borkenhagen@gmail.com>
2024-10-15 20:49:27 +0200

    fix: wrong typing for deletion dialog

--- cloud-dev-ui/src/pages/defects.vue
+++ cloud-dev-ui/src/pages/defects.vue
@@ -83,11 +83,11 @@
   <v-dialog v-model="confirmDialog" max-width="400">
     <v-card>
       <v-card-title>Confirm Delete</v-card-title>
-      <v-card-subtitle>Defect name: {{ toDelete.name }}</v-card-subtitle>
+      <v-card-subtitle>Defect name: {{ toDelete!.name }}</v-card-subtitle>
       <v-card-text> Are you sure you want to delete this defect?</v-card-text>
       <v-card-actions>
         <v-btn variant="text" @click="confirmDialog = false">Cancel</v-btn>
-        <v-btn color="error" @click="deleteDefect(toDelete.id)">Delete</v-btn>
+        <v-btn color="error" @click="deleteDefect(toDelete!.id)">Delete</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -187,7 +187,7 @@ function editDialog(defect: IDefect) {
   dialog.value = true;
 }
 
-function initiateDeletion(defect: Partial<IDefect> | undefined) {
+function initiateDeletion(defect: IDefect | undefined) {
   if (!defect) {
     console.error("No defect to delete");
     return;
