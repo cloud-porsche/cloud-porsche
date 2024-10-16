1216890f9802438079712c93a2e6113d9189c5fe
Ostabo <osi.borkenhagen@gmail.com>
2024-10-16 17:05:44 +0200

    feat: adjust cors rule for deployment

--- cloud-dev-property-management/src/main.ts
+++ cloud-dev-property-management/src/main.ts
@@ -4,11 +4,16 @@ import { ConfigService } from '@nestjs/config';
 import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [app.get(ConfigService).get('CORS_ORIGIN', 'localhost')],
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+  });
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
   await app.listen(app.get(ConfigService).get('PORT', 3000));
 }
+
 bootstrap();
