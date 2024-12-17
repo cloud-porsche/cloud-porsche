fb83c91cd594dbf3e23f0960a110f7e6262118c8
Ostabo <osi.borkenhagen@gmail.com>
2024-12-17 17:07:41 +0100

    fix(terraform): delete firestore dbs on destroy v2

--- tf/enterprise-tenant/gcloud-storage.tf
+++ tf/enterprise-tenant/gcloud-storage.tf
@@ -1,10 +1,10 @@
 resource "google_firestore_database" "enterprise_tenant" {
-  location_id             = "europe-west4"
-  name                    = "property-management-${var.tenant_id}"
-  type                    = "FIRESTORE_NATIVE"
-  concurrency_mode        = "PESSIMISTIC"
-  project                 = "cloud-porsche"
-  delete_protection_state = "DELETE"
+  location_id      = "europe-west4"
+  name             = "property-management-${var.tenant_id}"
+  type             = "FIRESTORE_NATIVE"
+  concurrency_mode = "PESSIMISTIC"
+  project          = "cloud-porsche"
+  deletion_policy  = "DELETE"
 }
 
 resource "google_storage_bucket" "enterprise_tenant" {
