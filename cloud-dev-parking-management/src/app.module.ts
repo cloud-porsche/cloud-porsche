81d9a09460daf2e4cdedc0d8e372a9563cdce10d
haasentimo <haasentimo@gmail.com>
2024-12-18 14:24:03 +0100

    feat(parking-management): adding some other files.

--- cloud-dev-parking-management/src/app.module.ts
+++ cloud-dev-parking-management/src/app.module.ts
@@ -6,11 +6,8 @@ import {
 } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DefectsModule } from './defects/defects.module';
 import { ConfigModule } from '@nestjs/config';
-import { ObjectStorageModule } from './object-storage/object-storage.module';
 import { AuthMiddleware } from './auth/auth.middleware';
-import { ParkingPropertiesModule } from './parking-properties/parking-properties.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ParkingModule } from './parking/parking.module';
 import { LoggingMiddleware } from './pubsub/logging.middleware';
@@ -18,13 +15,10 @@ import { PubSubModule } from './pubsub/pubsub.module';
 
 @Module({
   imports: [
-    DefectsModule,
-    ObjectStorageModule,
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: ['.env', '.env.production', '.env.development'],
     }),
-    ParkingPropertiesModule,
     ParkingModule,
     PubSubModule,
   ],
