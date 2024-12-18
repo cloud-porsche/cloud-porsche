0ae6faa5e07b0fd9f90232931a9d33162a1c22bb
Ostabo <osi.borkenhagen@gmail.com>
2024-12-18 12:17:55 +0100

    fix(terraform): add dependency if iam_member on service account

--- tf/enterprise-tenant/main.tf
+++ tf/enterprise-tenant/main.tf
@@ -27,16 +27,19 @@ resource "google_project_iam_member" "service_account_iam_datastore" {
   project = "cloud-porsche"
   role    = "roles/datastore.owner"
   member  = "serviceAccount:${google_service_account.tenant_service_account.email}"
+  depends_on = [google_service_account.tenant_service_account]
 }
 
 resource "google_project_iam_member" "service_account_iam_storage" {
   project = "cloud-porsche"
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.tenant_service_account.email}"
+  depends_on = [google_service_account.tenant_service_account]
 }
 
 resource "google_project_iam_member" "service_account_iam_pubsub" {
   project = "cloud-porsche"
   role    = "roles/pubsub.admin"
   member  = "serviceAccount:${google_service_account.tenant_service_account.email}"
+  depends_on = [google_service_account.tenant_service_account]
 }
