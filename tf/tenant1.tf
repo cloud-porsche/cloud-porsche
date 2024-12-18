84796720bc6f01564bdf3dc1ea48359de73ab5db
Ostabo <osi.borkenhagen@gmail.com>
2024-12-18 21:38:28 +0100

    test(ci): first tenant delete gh action

--- tf/tenant1.tf
+++ tf/tenant1.tf
@@ -1,15 +1,15 @@
 provider "helm" {
   alias = "tenant1"
   kubernetes {
-    host  = "https://${module.enterprise_tenant-1.cluster_endpoint}"
+    host  = "https://${module.tenant1.cluster_endpoint}"
     token = data.google_client_config.default.access_token
-    client_certificate = base64decode(module.enterprise_tenant-1.client_certificate)
-    client_key = base64decode(module.enterprise_tenant-1.client_key)
-    cluster_ca_certificate = base64decode(module.enterprise_tenant-1.cluster_ca_certificate)
+    client_certificate = base64decode(module.tenant1.client_certificate)
+    client_key = base64decode(module.tenant1.client_key)
+    cluster_ca_certificate = base64decode(module.tenant1.cluster_ca_certificate)
   }
 }
 
-module "enterprise_tenant-1" {
+module "tenant1" {
   source         = "./enterprise-tenant"
   tenant_id      = "tenant1"
   firebase_token = var.firebase_token
--- tf/tenant2.tf
+++ tf/tenant2.tf
@@ -1,15 +1,15 @@
 provider "helm" {
   alias = "tenant2"
   kubernetes {
-    host  = "https://${module.enterprise_tenant-2.cluster_endpoint}"
+    host  = "https://${module.tenant2.cluster_endpoint}"
     token = data.google_client_config.default.access_token
-    client_certificate = base64decode(module.enterprise_tenant-2.client_certificate)
-    client_key = base64decode(module.enterprise_tenant-2.client_key)
-    cluster_ca_certificate = base64decode(module.enterprise_tenant-2.cluster_ca_certificate)
+    client_certificate = base64decode(module.tenant2.client_certificate)
+    client_key = base64decode(module.tenant2.client_key)
+    cluster_ca_certificate = base64decode(module.tenant2.cluster_ca_certificate)
   }
 }
 
-module "enterprise_tenant-2" {
+module "tenant2" {
   source         = "./enterprise-tenant"
   tenant_id      = "tenant2"
   firebase_token = var.firebase_token
