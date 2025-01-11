52104485e2e505050d062dd51928d14553c9d0ee
Ostabo <osi.borkenhagen@gmail.com>
2025-01-11 16:19:25 +0100

    fix(helm): test subdomain only with managed cert

--- tf/enterprise-tenant/kubernetes-cluster.tf
+++ tf/enterprise-tenant/kubernetes-cluster.tf
@@ -1,6 +1,5 @@
 resource "google_dns_record_set" "tenant_domain" {
-  for_each = toset(["property-management", "parking-management", "monitoring-management"])
-  name = "${each.value}.${var.tenant_id}.cloud-porsche.com."
+  name = "${var.tenant_id}.cloud-porsche.com."
   type = "A"
   ttl  = 300
 
