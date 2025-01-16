226eb691d2270f62ac3421e80be26a78f3d4a991
Ostabo <osi.borkenhagen@gmail.com>
2025-01-16 16:20:06 +0100

    fix: set free-tier tenant tier to 0

--- tf/enterprise-tenant/firestore-tenant-info.tf
+++ tf/enterprise-tenant/firestore-tenant-info.tf
@@ -2,5 +2,5 @@ resource "google_firestore_document" "tenant_info" {
   collection  = "Tenants"
   database    = "(default)"
   document_id = var.tenant_id
-  fields      = "{\"tenantId\":{\"stringValue\":\"${var.tenant_id}\"},\"tenantName\":{\"stringValue\":\"${var.tenant_name}\"},\"tier\":{\"integerValue\":\"2\"},\"adminEmail\":{\"stringValue\":\"${var.admin_email}\"},\"ip\":{\"stringValue\":\"${trimsuffix(google_dns_record_set.tenant_domain.name, ".")}\"}}"
+  fields      = "{\"tenantId\":{\"stringValue\":\"${var.tenant_id}\"},\"tenantName\":{\"stringValue\":\"${var.tenant_name}\"},\"tier\":{\"integerValue\":\"${var.free_tier_overwrite ? 0 : 2}\"},\"adminEmail\":{\"stringValue\":\"${var.admin_email}\"},\"ip\":{\"stringValue\":\"${trimsuffix(google_dns_record_set.tenant_domain.name, ".")}\"}}"
 }
